ff7ef84571e2d5bcd5ff0cc602487c26a2b580f6
Lian Yelich <lianyelich14@gmail.com>
2020-01-14 22:53:19 +0000

    Created asyncBreeds.js and aded printCatBreed function

---
+++ asyncBreeds.js
@@ -0,0 +1,23 @@
+//@Lian(Driver)@Matthew@Calvin
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, cb) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
+    if (!error) {
+      cb(data, breed);
+    }
+  });
+  // ISSUE: Attempting to return data out here will also not work.
+  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
+};
+
+const printCatBreed = (data, breed) => {
+  console.log(`${breed}: ${data}`);
+};
+
+// we try to get the return value
+const bombay = breedDetailsFromFile('Bombay', printCatBreed);
